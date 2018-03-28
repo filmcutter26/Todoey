56fa20e56eb15237e7ad2b3535c5b594fe707196
Adam Viera <adam.viera@icloud.com>
2018-03-28 13:14:54 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Adam Viera on 3/28/18.
+//  Copyright © 2018 Adam Viera. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    
+    var done: Bool = false
+        
+}
